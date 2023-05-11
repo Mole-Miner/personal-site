53aea3cd50291f9f6d952ea7edb38f2d5ea2dd21
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-05-11 23:24:25 +0300

    |frontend core| added ngrx store devtools

--- frontend/projects/personal-site-core/src/lib/personal-site-core.module.ts
+++ frontend/projects/personal-site-core/src/lib/personal-site-core.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { API_URL } from "./tokens";
+
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: true,
+      traceLimit: 75
+    })
   ],
   providers: [
     {
       provide: API_URL,
       useValue: 'http://localhost:3000/api/v1'
     }
-  ]
+  ],
+  exports: [ StoreDevtoolsModule ]
 })
-export class PersonalSiteCoreModule { }
+export class PersonalSiteCoreModule {
+}
