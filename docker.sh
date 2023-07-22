a09289365d9919d06e3816fdd656388553309083
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-07-22 21:35:25 +0300

    |general| added start and stop docker commands

--- docker.sh
+++ docker.sh
@@ -10,8 +10,16 @@ docker_up() {
   eval "NG_PROJECT=${ng_project} docker-compose up -d"
 }
 
+docker_start() {
+  eval "docker-compose start"
+}
+
 docker_down() {
   eval "docker-compose down -v"
+}
+
+docker_stop() {
+  eval "docker-compose stop"
 }
 
 docker_prune() {
@@ -19,13 +27,17 @@ docker_prune() {
 }
 
 main() {
-  read -r -p "Enter docker command [build, up, down, prune] => " docker_command
+  read -r -p "Enter docker command [build, up, start, down, stop, prune] => " docker_command
   case ${docker_command} in
   "build") docker_build
     ;;
   "up") docker_up
   ;;
+  "start") docker_start
+  ;;
   "down") docker_down
+  ;;
+  "stop") docker_stop
   ;;
   "prune") docker_prune
   ;;
